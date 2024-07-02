280b1d426a814818888951b299320f747f7f7da0
Muhammad Irfan Fadilah <irfan.fadilah@inhealth.co.id>
2024-07-02 14:42:06 +0700

    add logic crazy button

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+let socket = io();
+
+const startingSection = document.querySelector(".starting-section");
+const homeBtn = document.querySelector(".home-btn");
+const crazyButton = document.getElementById("crazyButton");
+
+startButton.addEventListener("click", () => {
+  socket.emit("startGame");
+});
+
+homeBtn.addEventListener("click", () => {
+  window.location.href = "/";
+});
+
+socket.on("startGame", () => {
+  hideStartButton();
+});
+
+function hideStartButton() {
+  startButton.style.display = "none";
+  crazyButton.style.display = "block";
+  startingSection.style.display = "none";
+}
+
+crazyButton.addEventListener("click", () => {
+  socket.emit("crazyIsClicked", {
+    offsetLeft:
+      Math.random() * (window.innerWidth - crazyButton.clientWidth - 100),
+    offsetTop:
+      Math.random() * (window.innerHeight - crazyButton.clientHeight - 100),
+  });
+});
+
+socket.on("crazyIsClicked", (data) => {
+  goCrazy(data.offsetLeft, data.offsetTop);
+});
+
+function goCrazy(offLeft, offTop) {
+  let top, left;
+
+  left = offLeft;
+  too = offTop;
+
+  crazyButton.style.top = top + "px";
+  crazyButton.style.left = left + "px";
+  crazyButton.style.animation = "none";
+}
